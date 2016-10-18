5d1972aec7884de84ce2930657ab06baaf7dffe4
Ulises Bocchio <ulisesb@gmail.com>
2016-10-18 16:22:03 -0700

    upgraded to spring boot 1.4 and added web demo

--- jasypt-spring-boot-demo-web/src/main/java/sample/tomcat/SampleWebApplication.java
+++ jasypt-spring-boot-demo-web/src/main/java/sample/tomcat/SampleWebApplication.java
@@ -16,24 +16,14 @@
 
 package sample.tomcat;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@EnableEncryptableProperties
-public class SampleTomcatSslApplication extends SpringBootServletInitializer {
+public class SampleWebApplication {
 
-	public static void main(String[] args) throws Exception {
-                //System.setProperty("jasypt.encryptor.password", "password");
-		SpringApplication.run(SampleTomcatSslApplication.class, args);
-	}
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(SampleTomcatSslApplication.class);
-  }
+    public static void main(String[] args) throws Exception {
+        //System.setProperty("jasypt.encryptor.password", "password");
+        SpringApplication.run(SampleWebApplication.class, args);
+    }
 }
