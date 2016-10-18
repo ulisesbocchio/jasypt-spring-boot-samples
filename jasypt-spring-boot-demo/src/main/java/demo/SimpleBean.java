7af3fee6718f551f2fb181c75c11c8eb4180a423
Ulises Bocchio <ulisesb@gmail.com>
2016-10-17 17:28:48 -0700

    fixed xml context bean

--- jasypt-spring-boot-demo/src/main/java/demo/SimpleBean.java
+++ jasypt-spring-boot-demo/src/main/java/demo/SimpleBean.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * @author Ulises Bocchio, Sergio.U.Bocchio@Disney.com (BOCCS002)
@@ -10,6 +11,7 @@ public class SimpleBean {
 
   private static final Logger LOG = LoggerFactory.getLogger(SimpleBean.class);
 
+  @Value("${secret.property}")
   private String value;
 
   public String getValue() {
