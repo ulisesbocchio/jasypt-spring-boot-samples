eae9bf8e543f85bee207ace7d5c4d47b3ea677ba
Ulises Bocchio <Sergio.U.Bocchio@Disney.com>
2017-03-01 10:31:27 -0800

    spring boot 1.5

--- jasypt-spring-boot-demo-custom-resolver/src/test/java/demo/DemoApplicationTests.java
+++ jasypt-spring-boot-demo-custom-resolver/src/test/java/demo/DemoApplicationTests.java
@@ -2,11 +2,11 @@ package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = CustomResolverDemoApplication.class)
+@SpringBootTest(classes = CustomResolverDemoApplication.class)
 public class DemoApplicationTests {
 
 	static {
--- jasypt-spring-boot-demo-simple-starter/src/test/java/demo/DemoApplicationTests.java
+++ jasypt-spring-boot-demo-simple-starter/src/test/java/demo/DemoApplicationTests.java
@@ -2,11 +2,11 @@ package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SimpleStarterDemoApplication.class)
+@SpringBootTest(classes = SimpleStarterDemoApplication.class)
 public class DemoApplicationTests {
 
 	static {
